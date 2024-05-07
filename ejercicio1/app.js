b38ab326411816153e19f7c061a7e184d6d7173d
Nico Barroso <josenicolasbarrosogarcia@gmail.com>
2024-05-07 22:04:59 +0100

    Initial commit first exercise

---
+++ ejercicio1/app.js
@@ -0,0 +1,19 @@
+// 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+const btn = document.querySelector(".showme");
+console.log(btn);
+// 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+const heading = document.querySelector("#pillado");
+console.log(heading);
+// 1.3 Usa querySelector para mostrar por consola todos los p
+const allP = document.querySelectorAll("p");
+console.log(allP);
+// 1.4 Usa querySelector para mostrar por consola todos los elementos con
+// 	la clase.pokemon
+const isPokemon = document.querySelectorAll(".pokemon");
+console.log(isPokemon);
+// 1.5 Usa querySelector para mostrar por consola todos los elementos con
+// el atributo data-function="testMe".
+const testMe = document.querySelectorAll("[data-function='testMe']");
+console.log(testMe);
+// 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo
+// data-function="testMe".
